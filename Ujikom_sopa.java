afba6b0627c1d688b7b77f72d016ff2d5b3b199f
hilwah477 <wawahilwa522@gmail.com>
2025-04-14 14:33:31 +0700

    Add files via upload

---
+++ Ujikom_sopa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ujikom_sopa;
+
+/**
+ *
+ * @author USER
+ */
+public class Ujikom_sopa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
